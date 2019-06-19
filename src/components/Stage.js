29fbefafb3778482a5f2e84aab92c9cfe577cb91
Marior9 <mariorodriguezan@gmail.com>
2019-06-19 07:52:08 -0400

    new gamemap size refactor

--- src/components/Stage.js
+++ src/components/Stage.js
@@ -2,10 +2,10 @@ import React from 'react'
 import Rick from './Rick'
 import map from '../assets/testMap.png'
 import Morty from './Morty'
-import galaxy from '../assets/rickandmortygalaxy.png'
 
-let x = Math.floor(Math.random() * Math.floor(1550))
-let y = Math.floor(Math.random() * Math.floor(300))
+
+let x = Math.floor(Math.random() * Math.floor(window.innerWidth-600))
+let y = Math.floor(Math.random() * Math.floor(window.innerHeight-700))
 export default class Stage extends React.Component{
         constructor(){
             super()
@@ -15,6 +15,8 @@ export default class Stage extends React.Component{
                 currentMorty: 0,
                 mortyoffsetx: 0,
                 mortyoffsety: 0,
+                mapX: 0,
+                mapy: 0,
                 currentMortyToCatch: Math.floor(Math.random() * Math.floor(23))
             }
 
@@ -51,11 +53,11 @@ export default class Stage extends React.Component{
                 <div style={{
                     position: 'absolute',
                     top: '100px',
-                    left: '0px',
+                    left: '100px',
                     backgroundImage: `url(${map})`,
                     backgroundPosition: '0, 0',
-                    width: '100%',
-                    height: '65%',}} >
+                    width: '87%',
+                    height: '80%',}} >
                    
                 {<Rick renderNewMorty={this.props.renderNewMorty} handleMortyLocation={this.handleMortyLocation} handleRemoveMorty={this.props.handleRemoveMorty} newMorty={this.props.newMorty} handleNewMorty={this.props.handleNewMorty} currentMorty={this.state.currentMorty} mortyoffsetx={this.state.mortyoffsetx} mortyoffsety={this.state.mortyoffsety} mortyx={this.state.mortyLocationx} mortyy={this.state.mortyLocationy} rick={this.props.rick} morties={this.props.morties}/>}
                 <div  ref={this.myDiv} onClick={this.handleLocations} className="morty" style={{
